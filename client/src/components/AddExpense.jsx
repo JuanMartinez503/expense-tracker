163ba4f933437940d339cff31d434eba217c5f06
Juan Martinez <juanmartinez5678@hotmail.com>
2023-10-02 12:46:48 -0400

    I made the expenses component responsive

--- client/src/components/AddExpense.jsx
+++ client/src/components/AddExpense.jsx
@@ -4,9 +4,11 @@ export default function AddExpense() {
       <form action="" className="expenses-form">
       <h4 className="text-center mb-3">Add Expense</h4>
 
-        <input type="number" placeholder="Amount..." />
+       <div className="w-100 text-center">
+       <input type="number" placeholder="Amount..." />
         <input type="text" placeholder="Name..." />
         <input type="text" placeholder="Description..." />
+       </div>
         <button type="submit" className="btn btn-primary">
           Add Expense
         </button>
