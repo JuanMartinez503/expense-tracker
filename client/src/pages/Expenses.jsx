ab2f116af75b82bcedaee496dc04182a46c92ca3
Juan Martinez <juanmartinez5678@hotmail.com>
2023-10-02 11:28:17 -0400

    finished the styling for the expenses page

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -1,9 +1,23 @@
-
-export default function Expenses(){
-
-    return(
-        <div>
-            <h1>this is the expenses page</h1>
+export default function Expenses() {
+  return (
+    <div>
+            <div>
+        <div className="description-container">
+          <h3>Amount</h3>
+          <h3>Name</h3>
+          <h3>Description</h3>
+          
         </div>
-    )
+      
+      </div>
+      <div className="expense-update">
+    <input type="number" placeholder="this holds the amount" /><input type="text" placeholder="this holds the name" /><input type="text" />
+       
+      </div>
+      <div className="update-delete-btn text-center mt-2">
+          <button className="btn btn-warning">Update</button>
+          <button className="btn btn-danger">Delete</button>
+        </div>
+    </div>
+  );
 }
