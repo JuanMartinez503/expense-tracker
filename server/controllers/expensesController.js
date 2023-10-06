56d072df5d41b77ce017eace10401f25ad1d068f
Juan Martinez <juanmartinez5678@hotmail.com>
2023-10-06 14:09:43 -0400

    fixed a bug with the controller

--- server/controllers/expensesController.js
+++ server/controllers/expensesController.js
@@ -14,31 +14,36 @@ module.exports = {
   async createExpense(req, res) {
     try {
       const expense = await Expenses.create(req.body);
-      const user = await User.findOneAndUpdate({username:req.body.username}
-    ,
+      const user = await User.findOneAndUpdate(
+        { username: req.body.username },
         { $addToSet: { expenses: expense._id } },
         { new: true, runValidators: true }
       );
-      if (!user) {
-        return res
-          .status(404)
-          .json({ message: "Expense was created but no user was found" });
+  
+      if (!expense) {
+        console.log("Expense cannot be created");
+        return res.status(500).json({ message: "There was an error creating this Expense" });
       }
-      res.status(201).json({ message: "Expense was created successfully" });
+  
+      if (!user) {
+        return res.status(404).json({ message: "Expense was created but no user was found" });
+      }
+  
+      return res.status(201).json({ message: "Expense was created successfully" });
     } catch (err) {
-      res
-        .status(500)
-        .json({ message: "there was an error creating this Expense" }, err);
+      console.error(err);
+      return res.status(500).json({ message: "There was an error creating this Expense" });
     }
-  },
+  }
+  ,
   async updateExpense(req, res) {
     try {
       const expense = await Expenses.findByIdAndUpdate(req.params.expensesId,{$set:req.body},{new:true});
 
 
-      res.status(201).json({ message: "Expense was updated successfully" });
+     return  res.status(201).json({ message: "Expense was updated successfully" });
     } catch (err) {
-      res
+      return res
         .status(500)
         .json({ message: "there was an error updating this Expense" }, err);
     }
