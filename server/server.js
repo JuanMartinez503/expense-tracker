149051d6e9d60e85f6d9eedf4c0053277012bda9
Juan Martinez <juanmartinez5678@hotmail.com>
2023-10-08 14:38:57 -0400

    Fix Heroku build issue, I added a route to fix some routing issues

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist/index.html")));
 
 }
 app.use(routes);
